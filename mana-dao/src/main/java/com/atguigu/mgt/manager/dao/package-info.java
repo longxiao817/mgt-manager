cc900290c5e7bba69217c04aa1b8a5c14e2f153d
xiao <longxiao817@163.com>
2017-08-10 15:12:31 +0800

    初始环境

---
+++ mana-dao/src/main/java/com/atguigu/mgt/manager/dao/package-info.java
@@ -0,0 +1,37 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     com.atguigu.mgt.manager.dao
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     hao
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年8月8日 下午6:23:47
+ *
+ *    Revision:
+ *
+ *    2017年8月8日 下午6:23:47
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author hao
+ * @Date 2017年8月8日 下午6:23:47
+ * @version 1.0.0
+ */
+package com.atguigu.mgt.manager.dao;
---
+++ mana-service/src/main/java/com/atguigu/mgt/manager/service/package-info.java
@@ -0,0 +1,37 @@
+/******************************************************************
+ *
+ *    Java Lib For Android, Powered By Shenzhen Jiuzhou.
+ *
+ *    Copyright (c) 2001-2014 Digital Telemedia Co.,Ltd
+ *    http://www.d-telemedia.com/
+ *
+ *    Package:     com.atguigu.mgt.manager.service
+ *
+ *    Filename:    package-info.java
+ *
+ *    Description: TODO(用一句话描述该文件做什么)
+ *
+ *    Copyright:   Copyright (c) 2001-2014
+ *
+ *    Company:     Digital Telemedia Co.,Ltd
+ *
+ *    @author:     hao
+ *
+ *    @version:    1.0.0
+ *
+ *    Create at:   2017年8月8日 下午8:39:28
+ *
+ *    Revision:
+ *
+ *    2017年8月8日 下午8:39:28
+ *        - first revision
+ *
+ *****************************************************************/
+/**
+ * @ClassName package-info
+ * @Description TODO(这里用一句话描述这个类的作用)
+ * @author hao
+ * @Date 2017年8月8日 下午8:39:28
+ * @version 1.0.0
+ */
+package com.atguigu.mgt.manager.service;
